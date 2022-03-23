be499968417d9c2bf0c6241b986fba59639e05e6
Salman Dabbakuti <dabbakutisalman@gmail.com>
2022-03-23 23:00:45 +0530

    first commit

---
+++ contracts/MyCollectible.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MyCollectible is ERC721URIStorage {
+  
+    address public owner;
+    uint256 currentTokenId;
+    bool private isRevealed;
+
+    constructor() ERC721("Flower Collection", "FLWR") {
+        owner = msg.sender;
+    }
+
+    event Minted(uint256 currentTokenId, address indexed owner);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can perform this action");
+        _;
+    }
+
+    function mintToken(string memory _tokenURI) public onlyOwner {
+        _safeMint(msg.sender, currentTokenId);
+        _setTokenURI(currentTokenId, _tokenURI);
+        emit Minted(currentTokenId, msg.sender);
+        currentTokenId++;
+    }
+}
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,46 +1,23 @@
 import {
-  Approval as ApprovalEvent,
-  ApprovalForAll as ApprovalForAllEvent,
   Minted as MintedEvent,
   Transfer as TransferEvent
 } from "../generated/MyCollectible/MyCollectible"
-import { Approval, ApprovalForAll, Minted, Transfer } from "../generated/schema"
-
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
-  entity.save()
-}
-
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
-  entity.save()
-}
+import { Token } from "../generated/schema"
 
 export function handleMinted(event: MintedEvent): void {
-  let entity = new Minted(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.currentTokenId = event.params.currentTokenId
-  entity.owner = event.params.owner
-  entity.save()
+  let token = new Token(event.params.currentTokenId.toString())
+  token.tokenId = event.params.currentTokenId
+  token.owner = event.params.owner
+  token.updatedAtTimestamp = event.block.timestamp
+  token.save()
 }
 
 export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
-  entity.save()
+  let token = Token.load(event.params.tokenId.toString())
+  if (!token) {
+    token = new Token(event.params.tokenId.toString())
+  }
+  token.owner = event.params.to
+  token.updatedAtTimestamp = event.block.timestamp
+  token.save()
 }
